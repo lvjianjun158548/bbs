40139231b926ef04b16793c265ac7a69671b045b
lvjianjun158548 <1766133117@qq.com>
2019-10-31 19:09:38 +0800

    mysql

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '127.0.0.1',
+    'hostname'        => '39.100.238.214',
     // 数据库名
     'database'        => 'tpshop',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'root',
+    'password'        => 'ljj158548',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
